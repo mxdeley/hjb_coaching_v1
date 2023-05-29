d4498bf1673970e1bd80415e19c1f944c4b2a131
mxdeley <madeleydesignstudio@gmail.com>
2023-05-29 15:49:38 +0100

    Added Hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,16 +20,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Script id="hotjar">
-      (function(h,o,t,j,a,r){
+      <Script
+        id="hotjar"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:3511779,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-      </Script>
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`,
+        }}
+      ></Script>
 
       <body className={inter.className}>
         <Header />
