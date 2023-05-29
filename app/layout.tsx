494d69c8f458a68d60d8ee4dbd6adba7a20e8015
mxdeley <madeleydesignstudio@gmail.com>
2023-05-29 13:14:58 +0100

    Added Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import DesignedByInfinitim from "@/components/DesignedByInfinitim";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,9 +22,10 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
+        <DesignedByInfinitim />
+        <Analytics />
       </body>
-      <Footer />
-      <DesignedByInfinitim />
     </html>
   );
 }
