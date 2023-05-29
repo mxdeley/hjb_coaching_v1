91130f79d7570ba93276e763a69ed98ba0bef555
mxdeley <madeleydesignstudio@gmail.com>
2023-05-29 15:37:48 +0100

    Added Hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import DesignedByInfinitim from "@/components/DesignedByInfinitim";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3511779,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+      </Head>
       <body className={inter.className}>
         <Header />
         {children}
