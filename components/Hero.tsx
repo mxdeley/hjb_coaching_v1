44ae61c6dd4a98d3c866d519f5191146ad4d8f68
mxdeley <madeleydesignstudio@gmail.com>
2023-06-11 20:33:02 +0100

    changed hero section to amber-400

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -36,7 +36,7 @@ export default function Hero() {
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+          <h2 className="text-4xl font-bold tracking-tight text-amber-400 sm:text-6xl">
             Unlock your potential.
           </h2>
           <h3 className="mt-6 text-lg leading-8 text-white font-bold">
